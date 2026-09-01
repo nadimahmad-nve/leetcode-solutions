cef32a1bfa7e3ba3adab05f0952d935430cf69c6
nadimahmad-nve <nadimahmad10049@gmail.com>
2026-09-01 20:10:24 +0100

    Added Valid Parentheses solution

---
+++ Easy/ValidParentheses.cpp
@@ -0,0 +1,27 @@
+#include <stack> 
+#include <string> 
+
+using namespace std;
+
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char> stk; 
+        for (char c : s) {
+            if (c == '(' || c == '{' || c == '[') { 
+                stk.push(c);
+            } else { 
+                if (stk.empty()) return false; 
+                char top = stk.top();
+
+                if ((c == ')' && top == '(') || (c == '}' && top == '{') || (c == ']' && top == '[')) {
+                    stk.pop();
+                } else {
+                    return false; 
+                }
+            }
+        }
+
+        return stk.empty(); 
+    }
+};
