5f762ebed17775fc54c02d4a2957cd5ca5d12973
nadimahmad-nve <nadimahmad10049@gmail.com>
2026-09-02 21:31:17 +0100

    Added Linked List Cycle solution

---
+++ Easy/LinkedListCycle.cpp
@@ -0,0 +1,26 @@
+struct ListNode {
+   int val;
+   ListNode *next;
+   ListNode() : val(0), next(nullptr) {};
+   ListNode(int x) : val(x), next(nullptr) {};
+   ListNode(int x, ListNode *next) : val(x), next(next) {};
+};
+
+class Solution {
+public:
+    bool hasCycle(ListNode *head) {
+        ListNode* tortoise = head; 
+        ListNode* hare = head;
+
+        while (hare != nullptr && hare->next != nullptr) {
+            tortoise = tortoise->next; 
+            hare = hare->next->next; 
+
+            if (tortoise == hare) {
+                return true; 
+            }
+        }
+
+        return false; 
+    }
+};
