e9c3f78422027d8a4afddd670ea4fdb149e2c65b
nadimahmad-nve <nadimahmad10049@gmail.com>
2026-09-04 16:10:06 +0100

    Added Clone Graph solution

---
+++ Medium/CloneGraph.cpp
@@ -0,0 +1,44 @@
+#include <vector> 
+#include <unordered_map>
+
+using namespace std; 
+
+class Node {
+public:
+    int val;
+    vector<Node*> neighbors;
+    Node() {
+        val = 0;
+        neighbors = vector<Node*>();
+    }
+    Node(int _val) {
+        val = _val;
+        neighbors = vector<Node*>();
+    }
+    Node(int _val, vector<Node*> _neighbors) {
+        val = _val;
+        neighbors = _neighbors;
+    }
+};
+
+class Solution {
+private:
+    unordered_map<Node*, Node*> oldToNew;
+public:
+    Node* cloneGraph(Node* node) {
+        if (node == nullptr) { 
+            return nullptr; 
+        }
+
+        if (oldToNew.count(node)) return oldToNew[node];
+
+        Node* copy = new Node(node->val); 
+        oldToNew[node] = copy;
+
+        for(Node* neighbor : node->neighbors) { 
+            copy->neighbors.push_back(cloneGraph(neighbor)); 
+        }
+
+        return copy; 
+    }
+};
