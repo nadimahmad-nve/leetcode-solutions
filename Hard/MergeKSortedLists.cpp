980477c52eb9ae67a1e1620212a990bc97d83e4d
nadimahmad-nve <nadimahmad10049@gmail.com>
2026-09-08 16:32:41 +0100

    Added Merge K Sorted Lists solution

---
+++ Hard/MergeKSortedLists.cpp
@@ -0,0 +1,41 @@
+#include <vector>
+
+using namespace std; 
+
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+class Solution {
+public:
+    ListNode* mergeKLists(vector<ListNode*>& lists) {
+        if (lists.empty()) return nullptr;
+
+        int interval = 1; 
+        while (interval < lists.size()) { 
+            for (int i=0; i < lists.size()-interval; i+=interval*2) { 
+                lists[i] = mergeTwoLists(lists[i], lists[i+interval]); 
+            }
+            interval *= 2; 
+        }
+        
+        return lists[0]; 
+    }
+
+    ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) {
+        if (!l1) return l2; 
+        if (!l2) return l1; 
+
+        if (l1->val <= l2->val) { 
+            l1->next = mergeTwoLists(l1->next, l2); 
+            return l1; 
+        } else { 
+            l2->next = mergeTwoLists(l1, l2->next);
+            return l2;  
+        }
+    }
+};
